a4672ca583b8af45877f8c593780201208a74f85
ncd <staticd@gmail.com>
2010-02-27 14:09:50 -0500

    alpha release

--- Top40.py
+++ Top40.py
@@ -1,1 +1,89 @@
-/home/staticd/google.py
+#google.py
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#	Author: ncd
+#
+#	Title: google.py
+#
+#	Usage: >>> execfile('google.py') __or__ # python ./google.py
+#
+#	Description: Grab the top 40 list from dogstarradio.com and pump that list
+#	into google search and see if we can find some free mustic!
+#
+#	Version: 0.0.1
+#
+#	Distribution: I wrote it.  Everything is open source, therefore, this code 
+#	is open source.  If I am wrong, let me know by leaving a comment on my blog
+#	almostincongruent.blogspot.com
+#
+#	Notes:
+#	__in Usage:__
+#	>>> is your python prompt
+#	  # is your shell prompt
+#
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+import time
+import os
+import sys
+from xgoogle.search import GoogleSearch
+
+os.system('clear')
+
+def gethits():
+	import re, urllib2
+	response = urllib2.urlopen('http://dogstarradio.com/top40.php?channel=21')
+	html	 = response.read()
+	hitlist = ''.join(re.findall('<td>([a-zA-Z0-9 \n&]*)</td>',html))
+	hitlist_a = []
+	hitlist_b = ''
+	x = 0
+	for chr in hitlist:
+		if x == 40:
+			break
+		if chr.isdigit():
+			if hitlist_b == '\n':
+				hitlist_b = ''
+			if chr != '1' and hitlist_b != '':
+				hitlist_a.append(hitlist_b)
+				x += 1
+				hitlist_b = ''
+			continue	
+		hitlist_b += chr
+	return hitlist_a	
+	
+search = '* "free music" * music * download * '
+dont_search = ' -rapidshare -torrent -site:limewire.com -site:bearshare.com -site:*rapidshare*.com -site:*torrent*.com -site:abcmusic.net -site:kazaa.com'
+hot_bands = gethits()
+known_free = ['blink182','Smashing Pumpkins']
+
+for bnd in known_free:
+	hot_bands.append(bnd)
+
+file = open(os.path.expanduser('~/top40_' + str(int(time.time())) + '.txt'),'w') #make the file 'unique'
+
+for band in hot_bands:
+	band = '"' + band + '"'
+	gs = GoogleSearch(search + band + dont_search, random_agent=True)
+	gs.results_per_page = 10
+	time.sleep(10)
+	try: 
+		results = gs.get_results()
+	except:
+		print 'moving on',band
+		continue
+	print "searching: ",search + band + dont_search
+	count = 0
+	counter = 0
+	for res in results:
+		title = res.title.encode('utf8')
+		url   = res.url.encode('utf8') 
+		if count == 0:
+			file.write(':artist: __<b>' + band.strip('"') + '</b>__\n')
+			count += 1
+		file.write('<a href="' + str(url) + '"' + 'target="_blank">' + str(title) + '</a>\n')
+		counter += 1
+		if counter == 5:
+			break
+	time.sleep(.001) #Google does not like being harassed!
+
+file.close()		
