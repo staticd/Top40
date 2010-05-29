931e1a71f0492927f0e742b831c8b679791461ce
ncd <staticd@gmail.com>
2010-05-28 22:42:00 -0400

    code cleanup and addition of proper sleep time for successful, consistent access to Google

--- Top40.py
+++ Top40.py
@@ -36,11 +36,11 @@ from xgoogle.search import GoogleSearch
 
 os.system('clear')
 
-#this function gets the latest top 40 most played artists on sirius:alt nation
+# This function gets the latest top 40 most played artists on sirius:alt nation
 def gethits(): 
 	response = urllib2.urlopen('http://dogstarradio.com/top40.php?channel=21')
 	html = response.read()
-	#did not find '30 Seconds to Mars,' you found ' Seconds to Mars' ==must fix!
+	# did not find 'Thirty Seconds to Mars,' you found ' Seconds to Mars' ==must fix!
 	hitlist	= ''.join(re.findall('<td>([0-9 A-Za-z0-9 \n&]*)</td>',html)) 
 	hitlist_a = []
 	hitlist_b = ''
@@ -62,20 +62,19 @@ def gethits():
 	return hitlist_a
 	
 search = '* "free music" * music * download * '
-#keep updating with sites that are bad
+# keep updating with sites that are bad
 dont_search	= ' -rapidshare -torrent -site:limewire.com -site:*wire*.com -site:*wire.com -site:wire*.com -site:*share*.com -site:*share.com -site:share*.com -site:*torrent.com -site:torrent*.com -site:bearshare.com -site:*rapidshare*.com -site:*torrent*.com -site:abcmusic.net -site:kazaa.com -site:velocityreviews.com -site:globalshareware.com' 
 hot_bands = gethits()
-#bands that i know provide free music
-known_free = ['blink182','Smashing Pumpkins']
-sleepTime = random.randint(0,1000) 
+# bands that i know provide free music
+known_free = ['blink182','Smashing Pumpkins','Gregg Michael Gillis']
 
 for bnd in known_free:
 	hot_bands.append(bnd)
 
-#make the output file 'unique'
+# make the output file 'unique'
 file = open(os.path.expanduser('~/top40_' + str(int(time.time())) + '.txt'),'w')
 
-#Google only accepts 32 search parameters
+# Google only accepts 32 search parameters
 test_list = search + dont_search
 if len(test_list.split()) >= 31:
 	print "Google's search parameter limit (31) reached or exceeded"
@@ -85,10 +84,11 @@ for band in hot_bands:
 	
 	try:
 		gs = GoogleSearch(search + band + dont_search, random_agent=True)		
-		#can also be 25 or 100
-		gs.results_per_page = 10 
-		#adjust for experiment
-		time.sleep(sleepTime) 
+
+		# can also be 25 or 100
+		gs.results_per_page = 25 
+		# adjust for experiment
+		time.sleep(random.randint(0,1000)) 
 	except:
 		traceback.print_exc()
 	
@@ -100,22 +100,30 @@ for band in hot_bands:
 		continue
 	
 	print "searching: ",band
-	count   = 0
+	count = 0
 	counter = 0
-	
+
 	for res in results:
-		title = res.title.encode('utf8')
-		url   = res.url.encode('utf8') 
-		if count == 0:
-			file.write(':artist: __<b>' + band.strip('"') + '</b>__\n')
-			count += 1
-		file.write('<a href="' + str(url) + '"' + 'target="_blank">' + str(title) + '</a>\n')
-		counter += 1
-		#i only want the top five hits
-		if counter == 5: 
-			break
+		try:
+			title = res.title.encode("utf8")
+			url = res.url.encode("utf8") 
+			if count == 0:
+				file.write(':artist: __<b>' + band.strip('"') + '</b>__\n')
+				count += 1
+			file.write('<a href="' + str(url) + '"' + 'target="_blank">' + str(title) + '</a>\n')
+			counter += 1
+			# I only want the top five hits
+			if counter == 5: 
+				break
 	
-	#Google does not like being harassed!
-	time.sleep(sleepTime) 
+		# force file write instead of waiting for pipe size to be reached before write	
+#		file.flush()
+#		os.fsync(file)
+
+		except:
+			print traceback.print_exc()
+		
+		# Google does not like being harassed! --break glass in case of emergency
+		# time.sleep(random.randint(0,1000)) 
 
 file.close()		
