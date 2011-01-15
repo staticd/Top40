e70d506fb77a966a879f155ace53049988b31d92
Natt Day <staticd@gotti.(none)>
2011-01-14 20:34:10 -0500

    General code cleanup.

--- Top40.py
+++ Top40.py
@@ -36,11 +36,15 @@ from xgoogle.search import GoogleSearch
 
 os.system('clear')
 
+###############################################################################
 # This function gets the latest top 40 most played artists on sirius:alt nation
+###############################################################################
 def gethits(): 
 	response = urllib2.urlopen('http://dogstarradio.com/top40.php?channel=21')
 	html = response.read()
-	# did not find 'Thirty Seconds to Mars,' you found ' Seconds to Mars' ==must fix!
+	###########################################################################
+	# TODO: did not find 'Thirty Seconds to Mars,' you found ' Seconds to Mars'
+    ###########################################################################
 	hitlist	= ''.join(re.findall('<td>([0-9 A-Za-z0-9 \n&]*)</td>',html)) 
 	hitlist_a = []
 	hitlist_b = ''
@@ -62,38 +66,65 @@ def gethits():
 	return hitlist_a
 	
 search = '* "free music" * music * download * '
+
+###############################################################################
 # keep updating with sites that are bad
-dont_search	= ' -rapidshare -torrent -site:limewire.com -site:*wire*.com -site:*wire.com -site:wire*.com -site:*share*.com -site:*share.com -site:share*.com -site:*torrent.com -site:torrent*.com -site:bearshare.com -site:*rapidshare*.com -site:*torrent*.com -site:abcmusic.net -site:kazaa.com -site:velocityreviews.com -site:globalshareware.com' 
+###############################################################################
+dont_search	=	' -rapidshare'\
+				' -torrent'\
+				' -site:*wire*.com'\
+				' -site:*share*.com'\
+				' -site:*torrent*.com'\
+				' -site:abcmusic.net'\
+				' -site:kazaa.com'\
+				' -site:velocityreviews.com'\
+				' -site:globalshareware.com' 
+
 hot_bands = gethits()
+
+###############################################################################
 # bands that i know provide free music
-known_free = ['blink182','Smashing Pumpkins','Gregg Michael Gillis']
+###############################################################################
+known_free =	['blink182',
+				'Smashing Pumpkins',
+				'Gregg Michael Gillis',
+				'girl talk']
 
 for bnd in known_free:
 	hot_bands.append(bnd)
 
+###############################################################################
 # make the output file 'unique'
+###############################################################################
 file = open(os.path.expanduser('~/top40_' + str(int(time.time())) + '.txt'),'w')
 
+###############################################################################
 # Google only accepts 32 search parameters
+###############################################################################
 test_list = search + dont_search
+
 if len(test_list.split()) >= 31:
 	print "Google's search parameter limit (31) reached or exceeded"
-
+	sys.exit
+	
 for band in hot_bands:
 	band = '"' + band + '"'
+	print 'getting ready to search for band: ' + band
 	
 	try:
 		gs = GoogleSearch(search + band + dont_search, random_agent=True)		
-
-		# can also be 25 or 100
-		gs.results_per_page = 25 
-		# adjust for experiment
-		time.sleep(random.randint(0,1000)) 
+#test...
+#		print 'truth for gs',bool(gs)
+		gs.results_per_page = 25 # can also be 25 or 100
+		rand = random.randint(0,1000) # adjust for experiment
+		print 'sleeping for ' + str(rand) + ' seconds...'
+		time.sleep(rand)
 	except:
 		traceback.print_exc()
-	
 	try: 
 		results = gs.get_results()
+#test...
+#		print bool(results),len(results)
 	except:
 		print 'moving on',band
 		traceback.print_exc()
@@ -105,25 +136,22 @@ for band in hot_bands:
 
 	for res in results:
 		try:
+			#print type(res),'*',type(res.title)
 			title = res.title.encode("utf8")
 			url = res.url.encode("utf8") 
 			if count == 0:
 				file.write(':artist: __<b>' + band.strip('"') + '</b>__\n')
 				count += 1
+	#test...
+	#			print band
+	
 			file.write('<a href="' + str(url) + '"' + 'target="_blank">' + str(title) + '</a>\n')
 			counter += 1
-			# I only want the top five hits
-			if counter == 5: 
+			if counter == 5: # I only want the top five hits 
 				break
-	
-		# force file write instead of waiting for pipe size to be reached before write	
-#		file.flush()
-#		os.fsync(file)
-
+	#test...
+	#		print str(title),'**',str(url),'last title and url','counter',counter
 		except:
 			print traceback.print_exc()
-		
-		# Google does not like being harassed! --break glass in case of emergency
-		# time.sleep(random.randint(0,1000)) 
 
 file.close()		
